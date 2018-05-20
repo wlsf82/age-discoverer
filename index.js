6de6ce9a6018b22c6e0875280056bfa9ecf8d947
wlsf82 <wlsf82@gmail.com>
2018-05-20 20:37:42 +0200

    shorten code from index.js file

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 class AgeDiscoverer {
     findAgeByBornDate(yearOfBorn) {
         const currentYear = new Date().getFullYear();
-
         if (isNaN(yearOfBorn) || yearOfBorn > currentYear) {
             return 0;
         } else {
             const approximateAge = currentYear - yearOfBorn;
-
             return approximateAge;
         }
     }
