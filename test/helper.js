0eec6466030b9ce04d169c706406bcc9a79d268c
wlsf82 <wlsf82@gmail.com>
2018-05-27 00:49:57 +0200

    fix test helper method name

--- test/helper.js
+++ test/helper.js
@@ -1,5 +1,5 @@
 const getCurrentYear = function() { return new Date().getFullYear() };
-const calculateAproximateAgeBaseOnYearOfBorn = function(yearOfBorn) { return getCurrentYear() - yearOfBorn };
+const calculateAproximateAgeBasedOnYearOfBorn = function(yearOfBorn) { return getCurrentYear() - yearOfBorn };
 const calculateOneYearInTheFuture = function() { return getCurrentYear() + 1 };
 
-module.exports = { getCurrentYear, calculateAproximateAgeBaseOnYearOfBorn, calculateOneYearInTheFuture };
+module.exports = { getCurrentYear, calculateAproximateAgeBasedOnYearOfBorn, calculateOneYearInTheFuture };
